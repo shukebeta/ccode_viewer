4f18dbcc8684e33a9872c00f2459218f55fcd899
davidwei <davidwei@xps>
2025-10-11 07:28:26 +1300

    eed-history: feat: Configure Element Plus in main.js

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import './styles.css'
 import './styles/ui-light.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
